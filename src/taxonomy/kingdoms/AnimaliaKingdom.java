e967babf9c83c4434402444daf07c6969ad33874
Said AHEMT <said.ahemt@nolaroads.com>
2014-12-06 15:54:24 +0100

    ajoute l'age et l'état adulte avec getters / setters

--- src/taxonomy/kingdoms/AnimaliaKingdom.java
+++ src/taxonomy/kingdoms/AnimaliaKingdom.java
@@ -4,9 +4,35 @@ import java.util.ArrayList;
 
 public abstract class AnimaliaKingdom
 {
+    protected int           age = 0;
     protected ArrayList     descendents = new ArrayList();
+    protected boolean       isAdult = false;
+
+    /* Abstract methods */
 
     public abstract void    addDescendent();
     public abstract void    move();
     public abstract void    reproduce();
+
+    /* Getters */
+
+    public int              getAge()
+    {
+        return this.age;
+    }
+    public boolean          isAdult()
+    {
+        return this.isAdult;
+    }
+
+    /* Setters */
+
+    public void             setAge(int age)
+    {
+        this.age = age;
+    }
+    public void             setAdult(boolean isAdult)
+    {
+        this.isAdult = isAdult;
+    }
 }
