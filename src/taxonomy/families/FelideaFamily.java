69da970008d7c74b97e9bd4e87eea26ea98b83f6
Said AHEMT <said.ahemt@nolaroads.com>
2014-12-06 18:37:14 +0100

    ajoute la famille des félins (FelideaFamily)

---
+++ src/taxonomy/families/FelideaFamily.java
@@ -0,0 +1,15 @@
+package taxonomy.families;
+
+import taxonomy.classes.MammaliaClass;
+
+public abstract class FelideaFamily extends MammaliaClass
+{
+    public void purr()
+    {
+        System.out.println("Grrrrrrrrrrrr :)");
+    }
+    public void scratch()
+    {
+        System.out.println("Let me leave my signature on your skin...");
+    }
+}
