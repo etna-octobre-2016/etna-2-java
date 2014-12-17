00f4c611f636b2d67b9bc5ac71ae526a0f59647e
niou128 <niou128@gmail.com>
2014-12-17 14:10:47 +0100

    ajout de trucs dans une gazelle

--- src/taxonomy/model/species/Gazelle.java
+++ src/taxonomy/model/species/Gazelle.java
@@ -1,11 +1,16 @@
 package taxonomy.model.species;
 
+import taxonomy.behaviours.ILivingBehaviour;
 import taxonomy.model.families.BovidaeFamily;
+import taxonomy.model.life.LifeException;
+import taxonomy.state.ELifeState;
+import taxonomy.state.LifeStateMachine;
 
-public final class Gazelle extends BovidaeFamily
+public final class Gazelle extends BovidaeFamily implements ILivingBehaviour
 {
     private String name;
     private static String scientificName = "Gazella";
+    private ELifeState state;
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // Constructors
@@ -14,6 +19,7 @@ public final class Gazelle extends BovidaeFamily
     public Gazelle(String name)
     {
         this.name = name;
+        this.born();
     }
 
     public Gazelle()
@@ -54,6 +60,52 @@ public final class Gazelle extends BovidaeFamily
         System.out.println("I run away with grace and elegance");
     }
 
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // ILivingBehaviour interface methods definitions
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void born()
+    {
+        LifeStateMachine.changeState(this);
+    }
+
+    public void growUp()
+    {
+        LifeStateMachine.changeState(this, ELifeState.LIFEADULTE);
+    }
+
+    public void die()
+    {
+        LifeStateMachine.changeState(this, ELifeState.LIFEMORT);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // IStateChangeable interface methods definitions
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    public ELifeState getState()
+    {
+        return this.state;
+    }
+
+    public void setState(ELifeState state)
+    {
+        this.state = state;
+    }
+
+    public void validateStateChange(ELifeState nextState)
+    {
+        if (this.getState() == nextState)
+        {
+            throw new LifeException("This gazelle is already in the state: " + this.getState().getStateDescription());
+        }
+        if (this.getState() == ELifeState.LIFEADULTE && nextState == ELifeState.LIFENAISSANCE)
+        {
+            throw new LifeException("This gazelle cannot be a little gazelle again");
+        }
+    }
+    
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // Getters & Setters
     ///////////////////////////////////////////////////////////////////////////////////////////////
